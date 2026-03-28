2e1779283af7818e04eaaf28175463b46d19187f
Sarthak Mishra <mishrak85@gmail.com>
2026-03-29 01:02:41 +0530

    Removed unused imports

--- agent/rag_pipeline.py
+++ agent/rag_pipeline.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
 
